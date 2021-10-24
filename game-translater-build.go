9a663d015ef1464f6817c8643a2771a02f78de81
dj456119 <362088785@qq.com>
2021-10-24 12:09:20 +0800

    支持编译为windows程序

--- game-translater-build.go
+++ game-translater-build.go
@@ -6,7 +6,7 @@
  * @Author: cm.d
  * @Date: 2021-10-24 01:00:19
  * @LastEditors: cm.d
- * @LastEditTime: 2021-10-24 01:48:50
+ * @LastEditTime: 2021-10-24 12:09:00
  */
 package main
 
@@ -15,7 +15,24 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func Build() error {
+func Build(os string) error {
+
+	switch os {
+	case "windows":
+		return buildWindows()
+	case "macos":
+		return buildMac()
+	default:
+		logrus.Fatal("不支持的操作系统: ", os)
+	}
+
+	return nil
+}
+
+func buildMac() error {
+	env := make(map[string]string)
+	env["GOOS"] = "darwin"
+	env["GOARCH"] = "amd64"
 	logrus.Info("清理target目录")
 	sh.Run("rm", "-rf", "target")
 	logrus.Info("创建target目录")
@@ -23,7 +40,7 @@ func Build() error {
 		return err
 	}
 	logrus.Info("编译程序中")
-	if err := sh.Run("go", "build"); err != nil {
+	if err := sh.RunWith(env, "go", "build"); err != nil {
 		return err
 	}
 	logrus.Info("拷贝本地库")
@@ -50,6 +67,51 @@ func Build() error {
 	}
 	logrus.Info("清理中间文件")
 	if err := sh.Run("rm", "-rf", "game-translater"); err != nil {
+		return err
+	}
+	logrus.Info("已完成")
+	return nil
+}
+
+func buildWindows() error {
+	env := make(map[string]string)
+	env["GOOS"] = "windows"
+	env["GOARCH"] = "amd64"
+	logrus.Info("清理target目录")
+	sh.Run("rm", "-rf", "target")
+	logrus.Info("创建target目录")
+	if err := sh.Run("mkdir", "target"); err != nil {
+		return err
+	}
+	logrus.Info("编译程序中")
+	if err := sh.RunWith(env, "go", "build"); err != nil {
+		return err
+	}
+	logrus.Info("拷贝本地库")
+
+	if err := sh.Run("cp", "-rf", "./liblcl.dll", "./target"); err != nil {
+		return err
+	}
+	logrus.Info("拷贝程序")
+	if err := sh.Run("cp", "-rf", "./game-translater.exe", "./target"); err != nil {
+		return err
+	}
+	logrus.Info("拷贝配置文件")
+	if err := sh.Run("cp", "-rf", "aliyun-ocr-config.yaml", "./target"); err != nil {
+		return err
+	}
+	if err := sh.Run("cp", "-rf", "config.yaml", "./target"); err != nil {
+		return err
+	}
+	if err := sh.Run("cp", "-rf", "baidu-translater-config.yaml", "./target"); err != nil {
+		return err
+	}
+	logrus.Info("创建日志目录")
+	if err := sh.Run("mkdir", "./target/applog"); err != nil {
+		return err
+	}
+	logrus.Info("清理中间文件")
+	if err := sh.Run("rm", "-rf", "game-translater.exe"); err != nil {
 		return err
 	}
 	logrus.Info("已完成")
